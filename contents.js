7c48e031c38954cf8531f994e08d9a138126436a
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-01-27 00:42:00 +0900

    fix index

--- contents.js
+++ contents.js
@@ -1,13 +1,11 @@
 function books(){
   var z = document.getElementById('lang');
-      z.innerHTML = '<p>自己啓発本、論文、学書類</p>' + '<a href="https://yt5a.github.io/comic.html">漫画</a>' + '<p>ライトノベル</p>' + '<p>一般書籍</p>';
-  //document.getElementById("scroll").style.display="block";
+      z.innerHTML = '<a href="https://yt5a.github.io/comic.html">漫画</a>' + '<p>ライトノベル</p>' + '<p>一般書籍</p>';
 
 }
 
 function games(){
   var s = document.getElementById('lang');
-      s.innerHTML = '<p><a href="https://yt5a.github.io/sumahoge.html">ソーシャルゲーム</a></p>' + "<p>テレビゲーム</p>" + "<p>フリーゲーム</p>" ;
-  //document.getElementById("scroll").style.display="none";
+      s.innerHTML = '<a href="https://yt5a.github.io/sumahoge.html">ソーシャルゲーム</a>' + "<p>テレビゲーム</p>" + "<p>フリーゲーム</p>" ;
 
 }
