d100ede5fec8dccc97418a722ab2aa8ae75dd37a
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-02-04 14:18:39 +0900

    fix index

--- trpg-c.js
+++ trpg-c.js
@@ -1,8 +1,13 @@
-function ss(){
-  document.getElementById("str").innerHTML = rr();
-  document.getElementById("con").innerHTML = rr();
-  }
 
- rr(){
-  var a = 'tomato';
-  return a;
+  function ss(){
+a();
+b();
+  }
+  function a(){
+    var z = document.getElementById('str');
+        z.innerHTML = '<a href="https://yt5a.github.io/comic.html">漫画</a>' + '<p>ライトノベル</p>' + '<p><a href="https://yt5a.github.io/general-book.html">一般書籍</p></a>';
+  }
+  function b(){
+    var z = document.getElementById('con');
+        z.innerHTML = '<a href="https://yt5a.github.io/comic.html">漫画</a>' + '<p>ライトノベル</p>' + '<p><a href="https://yt5a.github.io/general-book.html">一般書籍</p></a>';
+  }
