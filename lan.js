738e08c071220fcb8f83d4aa3ac4889a7347c7f6
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-02-27 00:17:56 +0900

    fix index

--- lan.js
+++ lan.js
@@ -17,7 +17,6 @@ function onButtonClick() {
   for (var i = 1; i < tx0.length+1; i++) {
 
   if (i ==1) {
-  var datec = dateb.indexOf(tx0.charAt(i-1));
       dateb.push(tx0.charAt(i-1));
   var key = tx0.charAt(i-1);
       counts[key] = (counts[key])? counts[key] + 1 : 1 ;
@@ -88,6 +87,8 @@ word = "";
   table.insertBefore(moji, null);
   table.insertBefore(document.createElement('hr'), null);
 
+
+
   //テーブル3生成
   var moji= document.createElement('div');
   moji.setAttribute('id','three');
@@ -146,7 +147,218 @@ do {
   topsu1.setAttribute('id','1topsu' + ii);
   topsu1.innerHTML ="<span>" + onemojisu[i] + "</span>";
   three.insertBefore(topsu1, null);
-
   three.insertBefore(document.createElement('br'), null);
   }
+  var moji= document.createElement('span');
+  moji.innerHTML = 'top順';
+  table.insertBefore(moji, null);
+  table.insertBefore(document.createElement('hr'), null);
+  table.insertBefore(document.createElement('hr'), null);
+
+
+  //テーブル4生成
+  var moji= document.createElement('div');
+  moji.setAttribute('id','four');
+  moji.innerHTML ='<style>#four{width:200px;height:110px;overflow:auto;}</style>';
+  table.insertBefore(moji, null);
+
+
+  //各二文字単語数
+  var check = [];
+  var dateb = [];
+  for (var i = 1; i < tx0.length; i++) {
+  if (i ==1) {
+  var key = tx0.charAt(i-1)+tx0.charAt(i);
+      counts[key] = (counts[key])? counts[key] + 1 : 1 ;
+      dateb.push(tx0.charAt(i-1) + tx0.charAt(i));
+      check.push('c');
+  }
+  else {
+  var key = tx0.charAt(i-1)+tx0.charAt(i);
+      counts[key] = (counts[key])? counts[key] + 1 : 1 ;
+      dateb.push(tx0.charAt(i-1) + tx0.charAt(i));
+      check.push('c');
+ }
+
+}
+//各文字数
+ii = 0;
+for (var i = 1; i < tx0.length+1; i++) {
+if (dateb[i-1]!==undefined) {
+var ii = ii+1;
+var lenlan= document.createElement('span');
+lenlan.setAttribute('id','2lenlan' + ii);
+lenlan.innerHTML = ("[" + "<span id='2moji" + ii + "'>" + dateb[i-1] + "</span>" + "]" + "<span id='2mojisu" + ii + "'>" +counts[dateb[i-1]] + "</span>" + "<br>");
+four.insertBefore(lenlan, null);
+}
+var set = dateb[i-1];
+for (var f = 0; f < dateb.length+1; f++) {
+if (set==dateb[f]) {
+delete dateb[f];
+  }
+ }
+}
+//２文字単語数
+var moji= document.createElement('span');
+moji.innerHTML = '2文字単語数';
+table.insertBefore(moji, null);
+var len= document.createElement('span');
+len.setAttribute('id','2len');
+len.innerHTML =check.length;
+table.insertBefore(len, null);
+table.insertBefore(document.createElement('hr'), null);
+
+//2文字単語の数
+var moji= document.createElement('span');
+moji.innerHTML = '2文字単語種類';
+table.insertBefore(moji, null);
+var moji= document.createElement('span');
+moji.setAttribute('id','2tanngo');
+moji.innerHTML = ii;
+table.insertBefore(moji, null);
+table.insertBefore(document.createElement('hr'), null);
+
+
+//テーブル5生成
+var moji = document.createElement('div');
+moji.setAttribute('id','five');
+moji.innerHTML ='<style>#five{width:200px;height:110px;overflow:auto;}</style>';
+table.insertBefore(moji, null);
+
+//取得準備
+var topmoji = document.getElementById('2tanngo').innerHTML;
+var twomoji = [];
+var twomojisu = [];
+
+//取得
+for (var i = 0; i < topmoji; i++) {
+var ii = i+1;
+var twosu = '2moji' + ii;
+var twoji = '2mojisu' + ii;
+    twomoji.push(document.getElementById(twosu).innerHTML);
+    twomojisu.push(document.getElementById(twoji).innerHTML);
+ }
+ console.log(twomojisu)
+//編集
+do {
+var session = 0;
+for (var i = 0; i < topmoji; i++) {
+var a = twomojisu[i]
+var b = twomojisu[i+1]
+if (a*10 < b*10) {
+
+var c = twomojisu[i];
+var sc = twomoji[i];
+var d = twomojisu[i+1];
+var sd = twomoji[i+1];
+
+twomojisu[i] = d;
+twomoji[i] = sd;
+twomojisu[i+1] = c;
+twomoji[i+1] = sc;
+var session = 1;
+}
+}
+}while (session == 1);
+
+//添付
+var topmoji = document.getElementById('2tanngo').innerHTML;
+
+for (var i = 0; i < topmoji; i++) {
+var ii = i+1;
+
+
+var top2= document.createElement('span');
+top2.setAttribute('id','2top' + ii);
+top2.innerHTML = "[" + "<span>" + twomoji[i] + "</span>" + "]";
+five.insertBefore(top2, null);
+
+var topsu2= document.createElement('span');
+topsu2.setAttribute('id','2topsu' + ii);
+topsu2.innerHTML ="<span>" + twomojisu[i] + "</span>";
+five.insertBefore(topsu2, null);
+five.insertBefore(document.createElement('br'), null);
+
+}
+
+var moji= document.createElement('span');
+moji.innerHTML = 'top順';
+table.insertBefore(moji, null);
+table.insertBefore(document.createElement('hr'), null);
+table.insertBefore(document.createElement('hr'), null);
+
+
+
+
+
+
+
+
+
+
+
+
+//テーブル6生成
+var moji= document.createElement('div');
+moji.setAttribute('id','six');
+moji.innerHTML ='<style>#six{width:200px;height:110px;overflow:auto;}</style>';
+table.insertBefore(moji, null);
+
+
+//各3文字単語数
+var check = [];
+var dateb = [];
+for (var i = 1; i < tx0.length-1; i++) {
+if (i ==1) {
+var key = tx0.charAt(i-1)+tx0.charAt(i)+tx0.charAt(i+1);
+    counts[key] = (counts[key])? counts[key] + 1 : 1 ;
+    dateb.push(tx0.charAt(i-1) + tx0.charAt(i) + tx0.charAt(i+1));
+    check.push('c');
+}
+else {
+var key = tx0.charAt(i-1)+tx0.charAt(i)+tx0.charAt(i+1);
+    counts[key] = (counts[key])? counts[key] + 1 : 1 ;
+    dateb.push(tx0.charAt(i-1) + tx0.charAt(i) + tx0.charAt(i+1));
+    check.push('c');
+}
+
+}
+//各文字数
+ii = 0;
+for (var i = 1; i < tx0.length+1; i++) {
+if (dateb[i-1]!==undefined) {
+var ii = ii+1;
+var lenlan= document.createElement('span');
+lenlan.setAttribute('id','3lenlan' + ii);
+lenlan.innerHTML = ("[" + "<span id='3moji" + ii + "'>" + dateb[i-1] + "</span>" + "]" + "<span id='3mojisu" + ii + "'>" +counts[dateb[i-1]] + "</span>" + "<br>");
+six.insertBefore(lenlan, null);
+}
+var set = dateb[i-1];
+for (var f = 0; f < dateb.length+1; f++) {
+if (set==dateb[f]) {
+delete dateb[f];
+  }
+ }
+}
+//２文字単語数
+var moji= document.createElement('span');
+moji.innerHTML = '3文字単語数';
+table.insertBefore(moji, null);
+var len= document.createElement('span');
+len.setAttribute('id','3len');
+len.innerHTML =check.length;
+table.insertBefore(len, null);
+table.insertBefore(document.createElement('hr'), null);
+
+//2文字単語の数
+var moji= document.createElement('span');
+moji.innerHTML = '3文字単語種類';
+table.insertBefore(moji, null);
+var moji= document.createElement('span');
+moji.setAttribute('id','3tanngo');
+moji.innerHTML = ii;
+table.insertBefore(moji, null);
+table.insertBefore(document.createElement('hr'), null);
+
+
 }
