fc6abfe19474f69984010c6ea63132190082141e
山田　宇矩 <yamadtakanori@yamadasakainorinoMacBook-puro.local>
2019-03-07 12:19:50 +0900

    fix index

---
+++ dot-painter.js
@@ -0,0 +1,32 @@
+
+  (function() {
+    document.addEventListener('DOMContentLoaded', function() {
+      document.getElementById('convert-button').addEventListener('click', function() {
+        svg2imageData(document.getElementById('svg'), function(data) {
+          console.log(data);
+          document.getElementById('converted-image').src = data;
+        }, function(error) {
+          console.log(error);
+          alert('failed to convert');
+        });
+      });
+    });
+
+
+    function svg2imageData(svgElement, successCallback, errorCallback) {
+      var canvas = document.createElement('canvas');
+      canvas.width = svgElement.width.baseVal.value;
+      canvas.height = svgElement.height.baseVal.value;
+      var ctx = canvas.getContext('2d');
+      var image = new Image();
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, image.width, image.height);
+        successCallback(canvas.toDataURL());
+      };
+      image.onerror = (e) => {
+        errorCallback(e);
+      };
+      var svgData = new XMLSerializer().serializeToString(svgElement);
+      image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa(svgData);
+    }
+  }());
